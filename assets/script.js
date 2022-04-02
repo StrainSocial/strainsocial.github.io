22665f59b519648bff948fd5675b1481ef6ebf5b
geosjobby <geoiscool1213@gmail.com>
2022-04-02 19:47:41 +0100

    site completed i think

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+function a() {
+    navigator.clipboard.writeText('StrainSocial#7579')
+    window.alert("Copied 'StrainSocial#7579' to Clipboard!");
+  }
+  (window.onload = onLoad), (window.onresize = onResize);
